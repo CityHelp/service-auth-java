baf94969ff8eb9363bdb772135279f973be52a4b
JuanMendez98 <Juancamilosanchezmendez@gmail.com>
2025-11-28 17:03:53 -0500

    feat: Initialize project with Maven, Spring Boot setup, and basic application structure

---
+++ src/main/java/com/crudzaso/CityHelp/CityHelpApplication.java
@@ -0,0 +1,13 @@
+package com.crudzaso.CityHelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityHelpApplication.class, args);
+	}
+
+}
